4d0a3423b370438a88690cfdf1082b2700b53b65
Slenso <113910069+Slenso@users.noreply.github.com>
2022-09-19 10:28:03 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,13 +4,14 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Meu prof. é Incrível!")
+st.title("É MEC OU NÃO É?!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
+st.header("Dimas")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("Sub Dimas")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais! diferentemente da função apresentada pela vávula solenóia")
+st.write("entao minha rapziada curso de mecanica ele conhecido como gracha, porque? porque a gente gosta do gosto do ferro e se achar ruim pode vir trocar um soco com esse amigo aqui bem conhecido no bentao como dudão e seus zamigos da firma.... onbrigado pela atencao vao todos arranjar algo melhor pra fazer do que ficar lendo essa pequena mensagem ")
